7dae1ee5ef674e40a0304250a259e68268228eb5
cColds <103373668+cColds@users.noreply.github.com>
2023-02-02 23:17:31 -0600

    Refactor handleGIF to async/await

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const search = document.querySelector(".search");
 const keyword = document.querySelector(".keyword");
 const img = document.querySelector("img");
 
-function handleGIF() {
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=UrbD4m6kVRvnpdqaMw2LzqBgVsojJHxq&s=${searchBar.value}`,
-		{ mode: "cors" }
-	)
-		.then((response) => response.json())
-		.then((response) => {
-			img.src = response.data.images.original.url;
-			keyword.textContent = searchBar.value;
-			img.classList.add("active");
-		})
-		.catch(() => {
-			console.error("GIF not found");
-			keyword.textContent = `No GIFS found for "${searchBar.value}"`;
-			img.src = "";
-			img.classList.remove("active");
-		});
+async function handleGIF() {
+	try {
+		const response = await fetch(
+			`https://api.giphy.com/v1/gifs/translate?api_key=UrbD4m6kVRvnpdqaMw2LzqBgVsojJHxq&s=${searchBar.value}`,
+			{ mode: "cors" }
+		);
+		const gifData = await response.json();
+		img.src = gifData.data.images.original.url;
+		keyword.textContent = searchBar.value;
+		img.classList.add("active");
+	} catch {
+		console.error("GIF not found");
+		keyword.textContent = `No GIFS found for "${searchBar.value}"`;
+		img.src = "";
+		img.classList.remove("active");
+	}
 }
 
 search.addEventListener("click", handleGIF);
